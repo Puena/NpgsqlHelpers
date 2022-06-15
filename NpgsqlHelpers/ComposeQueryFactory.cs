e441eeea86187de8b62727137cb9be1761f29973
Dmitriy Platonov <woweuropeluffy@gmail.com>
2022-06-15 23:06:55 +0300

    Homework 2022-06-15

---
+++ NpgsqlHelpers/ComposeQueryFactory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpgsqlHelpers
+{
+    internal static class ComposeQueryFactory
+    {
+        private static readonly char[] _stopChars = new char[] { ' ', ',', ':', ')', '\t', '\n', ';' };
+        private static readonly char _atSign = '@';
+
+        /// <summary>
+        /// Compose Npgsql query from user query!
+        /// </summary>
+        /// <param name="initQuery"></param>
+        /// <returns>Return <see cref="ParsedQuery">ParsedQuery record</see>.</returns>
+        /// <exception cref="ArgumentNullException">Throw exception if <paramref name="initQuery"/> is null</exception>
+        internal static ParsedQuery ParseQuery(string initQuery)
+        {
+            if (string.IsNullOrEmpty(initQuery)) throw new ArgumentNullException(nameof(initQuery), "Query can not be null or empy string!");
+
+            StringBuilder _parsedQuery = new();
+            StringBuilder _fieldName = new();
+            List<string> _fieldsNames = new();
+            bool start = false;
+            int position = 0;
+
+            foreach (char c in initQuery)
+            {
+                if (start)
+                {
+                    if (_stopChars.Any(sc => sc == c))
+                    {
+                        _fieldsNames.Add(_fieldName.ToString());
+                        _parsedQuery.Append(c);
+                        start = false;
+                        _fieldName.Clear();
+                    }
+                    else
+                    {
+                        _fieldName.Append(c);
+                    }
+                }
+                else if (c == _atSign)
+                {
+                    start = true;
+                    position++;
+                    _parsedQuery.Append($"${position}");
+                }
+                else
+                {
+                    _parsedQuery.Append(c);
+                }
+            }
+
+            if (start)
+            {
+                _fieldsNames.Add(_fieldName.ToString());
+                _fieldName.Clear();
+            }
+
+            return new ParsedQuery(initQuery, _parsedQuery.ToString(), _fieldsNames.ToArray());
+        }
+    }
+}
---
+++ NpgsqlHelpers/ParsedQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpgsqlHelpers
+{
+    /// <summary>
+    /// Represent parsed string object
+    /// </summary>
+    /// <param name="InitQuery">User defined query, like: SELECT * FROM shops WHERE Id = @Id</param>
+    /// <param name="NpgsqlQuery">Parsed query for npgsql format, like: SELECT * FROM shops WHERE Id = $1</param>
+    /// <param name="ParamNames">Parameters name with strict order, like: ["Id"]</param>
+    internal record ParsedQuery(string InitQuery, string NpgsqlQuery, string[] ParamNames);
+}
